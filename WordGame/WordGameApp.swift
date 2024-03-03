eaa5acd54ad235fc40a77a6de546586afb48bfb5
Daniel Höfling <dan.hoefi@web.de>
2024-03-03 12:38:32 +0100

    Initial Commit

---
+++ WordGame/WordGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGameApp.swift
+//  WordGame
+//
+//  Created by Daniel on 03.03.24.
+//
+
+import SwiftUI
+
+@main
+struct WordGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
